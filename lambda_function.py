a61800020e7dd62e580706f8c6df220fb3408e30
Ananth Sivaramakrishnan <ananth.sivaramakrishnan@one.verizon.com>
2018-07-08 20:42:43 -0400

    Added logs -This lambda function, should log the following information into cloudwatch logs File name File size File location in S3 Number of words in the text file

--- lambda_function.py
+++ lambda_function.py
@@ -6,7 +6,10 @@ def lambda_handler(event, context):
         file_obj = event["Records"][0]
         filename = str(file_obj['s3']['object']['key'])
         filename = urllib.parse.unquote_plus(filename)
-        print("Filename: ", filename)
         fileObj = s3.get_object(Bucket = "aws-s3-upload-bucket", Key="upload/Greetings.txt")
         file_content = fileObj["Body"].read().decode('utf-8')
-        print(file_content)
+        print("File Content: ", file_content)
+        print("File Name: ", str(file_obj['s3']['object']['key']))
+        print("File Size: ", str(file_obj['s3']['object']['size']), "Bytes")
+        print("Number of words in the text file: ",len(file_content.split()))
+        print("File location in S3: ", str(file_obj['s3']['bucket']['name']),"/",str(file_obj['s3']['object']['key']))
